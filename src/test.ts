0374b8d951a534b4dd50ad620b43d02a3d8980cf
Corrado Amatore <>
2021-03-20 23:16:04 +0100

    remove module

--- src/test.ts
+++ src/test.ts
@@ -1,34 +1,32 @@
-import { Downloader } from ".";
-
-//const {Downloader} = require('./index')
+import { DebugMode, Downloader, File, Mode, Options, Status } from ".";
 
 const fileURL =  "https://storage.googleapis.com/gd-wagtail-prod-assets/original_images/evolving_google_identity_2x1.jpg"
 
 var files =  [
-  new Downloader.File(fileURL, "./download/image.jpg"),
-  new Downloader.File(fileURL, "./download/0/image.jpg"),
-  new Downloader.File(fileURL, "./download/1/image.jpg"),
-  new Downloader.File(fileURL, "./download/2/image.jpg"),
-  new Downloader.File(fileURL, "./download/3/image.jpg"),
-  new Downloader.File(fileURL, "./download/4/image.jpg"),
-  new Downloader.File(fileURL, "./download/5/image.jpg"),
-  new Downloader.File(fileURL, "./download/6/image.jpg"),
-  new Downloader.File(fileURL, "./download/7/image.jpg"),
-  new Downloader.File(fileURL, "./download/8/image.jpg"),
-  new Downloader.File(fileURL, "./download/9/image.jpg")
+  new File(fileURL, "./download/image.jpg"),
+  new File(fileURL, "./download/0/image.jpg"),
+  new File(fileURL, "./download/1/image.jpg"),
+  new File(fileURL, "./download/2/image.jpg"),
+  new File(fileURL, "./download/3/image.jpg"),
+  new File(fileURL, "./download/4/image.jpg"),
+  new File(fileURL, "./download/5/image.jpg"),
+  new File(fileURL, "./download/6/image.jpg"),
+  new File(fileURL, "./download/7/image.jpg"),
+  new File(fileURL, "./download/8/image.jpg"),
+  new File(fileURL, "./download/9/image.jpg")
 ]
 
-const options = {
+const options: Options = {
     retry_times: 3,
-    mode: Downloader.Mode.SYNC,
-    debug_mode: Downloader.DebugMode.DEBUG
+    mode: Mode.SYNC,
+    debug_mode: DebugMode.DEBUG
 };
 
 async function test(){
-    var downloader = new Downloader.Downloader(options)
-    await downloader.run(files, function(output: Downloader.File[]){
-        var downloadedCount = output.filter(x=>x.response?.status == Downloader.Status.OK).length;
-        let errorCount = output.filter(x=>x.response?.status == Downloader.Status.KO).length;
+    var downloader = new Downloader(options)
+    await downloader.run(files, function(output: File[]){
+        var downloadedCount = output.filter(x=>x.response?.status == Status.OK).length;
+        let errorCount = output.filter(x=>x.response?.status == Status.KO).length;
         console.debug("Downloaded " + downloadedCount + " files with " + errorCount + " errors");
     })
     /* downloader.run(files, function(output: Downloader.File[]){
--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
-const {Downloader} = require('../dist/src/index')
+const {Downloader, File, Mode, DebugMode, Status} = require('../dist/src/index')
 
 const fileURL =  "https://storage.googleapis.com/gd-wagtail-prod-assets/original_images/evolving_google_identity_2x1.jpg"
 
 var files =  [
-  new Downloader.File(fileURL, "./download/image.jpg"),
-  new Downloader.File(fileURL, "./download/0/image.jpg"),
-  new Downloader.File(fileURL, "./download/1/image.jpg"),
-  new Downloader.File(fileURL, "./download/2/image.jpg"),
-  new Downloader.File(fileURL, "./download/3/image.jpg"),
-  new Downloader.File(fileURL, "./download/4/image.jpg"),
-  new Downloader.File(fileURL, "./download/5/image.jpg"),
-  new Downloader.File(fileURL, "./download/6/image.jpg"),
-  new Downloader.File(fileURL, "./download/7/image.jpg"),
-  new Downloader.File(fileURL, "./download/8/image.jpg"),
-  new Downloader.File(fileURL, "./download/9/image.jpg")
+  new File(fileURL, "./download/image.jpg"),
+  new File(fileURL, "./download/0/image.jpg"),
+  new File(fileURL, "./download/1/image.jpg"),
+  new File(fileURL, "./download/2/image.jpg"),
+  new File(fileURL, "./download/3/image.jpg"),
+  new File(fileURL, "./download/4/image.jpg"),
+  new File(fileURL, "./download/5/image.jpg"),
+  new File(fileURL, "./download/6/image.jpg"),
+  new File(fileURL, "./download/7/image.jpg"),
+  new File(fileURL, "./download/8/image.jpg"),
+  new File(fileURL, "./download/9/image.jpg")
 ]
 
 
@@ -23,14 +23,14 @@ describe('Array', function() {
     it('Download 11 files', async () => {
       const options = {
           retry_times: 3,
-          mode: Downloader.Mode.SYNC,
-          debug_mode: Downloader.DebugMode.DEBUG
+          mode: Mode.SYNC,
+          debug_mode: DebugMode.DEBUG
       };
       
-      var downloader = new Downloader.Downloader(options);
+      var downloader = new Downloader(options);
       await downloader.run(files, function (output) {
-          var errorCount = output.filter(function (x) { var _a; return ((_a = x.response) === null || _a === void 0 ? void 0 : _a.status) == Downloader.Status.KO; }).length;
-          var downloadedCount = output.filter(function (x) { var _a; return ((_a = x.response) === null || _a === void 0 ? void 0 : _a.status) == Downloader.Status.OK; }).length;
+          var errorCount = output.filter(function (x) { var _a; return ((_a = x.response) === null || _a === void 0 ? void 0 : _a.status) == Status.KO; }).length;
+          var downloadedCount = output.filter(function (x) { var _a; return ((_a = x.response) === null || _a === void 0 ? void 0 : _a.status) == Status.OK; }).length;
           console.debug("Downloaded " + downloadedCount + " files with " + errorCount + " errors");
           assert.strictEqual(downloadedCount, 11);
         });
